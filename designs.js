eeca7d635d13d3f59847da8f32cc7e0e2d74becb
DraciVik <viktor.stojanov90@gmail.com>
2020-02-27 20:07:09 +0100

    Added makeGrid function to make a table grid based on user input

---
+++ designs.js
@@ -0,0 +1,51 @@
+// Select color input
+let colorInput = document.querySelector("input[type='color']");
+
+let color = colorInput.value;
+
+colorInput.addEventListener("change", function(e) {
+  color = e.target.value;
+});
+
+// Select size input
+const gridHeightInput = document.querySelector("#inputHeight");
+let heightValue = gridHeightInput.value;
+
+const gridWidthInput = document.querySelector("#inputWidth");
+let widthValue = gridWidthInput.value;
+
+const form = document.querySelector("#sizePicker");
+
+form.addEventListener("change", function(e) {
+  if (e.target === gridHeightInput) {
+    heightValue = e.target.value;
+  } else if (e.target === gridWidthInput) {
+    widthValue = e.target.value;
+  }
+});
+
+// When size is submitted by the user, call makeGrid()
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  makeGrid(heightValue, widthValue);
+});
+
+function makeGrid(height, width) {
+  // Your code goes here!
+  const pixelCanvas = document.querySelector("#pixelCanvas");
+  const table = document.createElement("table");
+  for (let i = 0; i < height; i += 1) {
+    let tableRow = document.createElement("tr");
+    for (let y = 0; y < width; y += 1) {
+      let tableData = document.createElement("td");
+      tableRow.appendChild(tableData);
+    }
+    table.appendChild(tableRow);
+  }
+  pixelCanvas.appendChild(table);
+}
+
+// pixelCanvas.addEventListener("click", function(e) {
+
+// })
